f52e1b186d4823024e069e55f774e4e7e5d08dc7
Joshua Sherman <josh@gravityblvd.com>
2014-01-20 23:51:01 -0500

    Added composer.json

---
+++ composer.php
@@ -0,0 +1,14 @@
+{
+	"name":        "joshtronic/php-googleplaces",
+	"description": "PHP Wrapper for the Google Places API ",
+	"license":     "MIT",
+	"authors": [
+		{
+			"name": "Joshua Sherman",
+			"email": "josh@gravityblvd.com"
+		}
+	],
+	"autoload": {
+		"classmap": ["GooglePlaces.php"]
+	}
+}
