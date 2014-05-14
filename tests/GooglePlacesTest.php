c3fe6e8e5934f3274e331f8751da92cea15fd57a
Josh Sherman <josh@gravityblvd.com>
2014-05-14 17:35:25 -0400

    Started to set up tests, added configs

---
+++ tests/GooglePlacesTest.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once '../src/GooglePlaces.php';
+
+class GooglePlacesTest extends PHPUnit_Framework_TestCase
+{
+	public function testNearbySearchProximity()
+	{
+
+	}
+
+	public function testNearbySearchDistance()
+	{
+
+	}
+
+	public function testNearbySearchPagination()
+	{
+
+	}
+
+	public function testRadarSearch()
+	{
+
+	}
+
+	public function testDetails()
+	{
+
+	}
+}
+
+?>
